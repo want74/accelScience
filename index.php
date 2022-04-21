3132af9d7854f88094c98a31b6e74d00d562ff82
want74 <want74@rambler.ru>
2022-04-21 22:50:19 +0900

    Косметика

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <title>AccelScience</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
-
+        /*Ну боди, чел ты чё?*/
         body {
             margin: 0 auto;
             height: auto;
@@ -24,7 +24,7 @@
             background-attachment: fixed;
             background-size: cover;
         }
-
+        /*То что у нас программа?*/
         .spec {
             font-family: 'Open Sans', sans-serif;
             font-style: normal;
@@ -36,7 +36,7 @@
             text-align: center;
             letter-spacing: 0.12em;
         }
-
+        /*Название нашего прокта*/
         .ourProject {
             font-family: 'Open Sans', sans-serif;
             font-style: normal;
@@ -48,7 +48,7 @@
             text-align: center;
             letter-spacing: 0.12em;
         }
-
+        /*Слоган проекта*/
         .slogan {
             font-family: 'Open Sans', sans-serif;
             font-style: normal;
@@ -60,7 +60,7 @@
             text-align: center;
             letter-spacing: 0.12em;
         }
-
+        /*кнопка выбора программы*/
         .btn-choice {
             background: #8080FF;
             border-radius: 4px;
@@ -72,7 +72,7 @@
             font-size: 18px;
             line-height: 20px;
         }
-
+        /*Первый текст во втором слайде*/
         .f-text {
             font-family: 'Open Sans', sans-serif;
             font-style: normal;
@@ -85,7 +85,7 @@
 
             color: #8080FF;
         }
-
+        /*Второй текст во втором слайде*/
         .s-text {
             font-family: 'Open Sans';
             font-style: normal;
@@ -98,32 +98,144 @@
 
             color: #8080FF;
         }
-
+        /* кнопка бургера*/
         .btn-burger {
             border: 0;
         }
-
+        /*Иконка бургера*/
         .navbar-toggler-icon {
             background-image: url(img/burger.svg) !important;
+        }
+        /*Стили навигатора*/
+        .showed {
+            position: fixed !important;
+            background: rgba(115, 124, 132, 0.04);
+            backdrop-filter: blur(15px);
+            height: 100vh;
+            position: absolute;
+            left: -33vw;
+            z-index: 100;
+        }
+        /*Анимация появления навигатора*/
+        .animate {
+            margin-left: 43vw;
+            transition-property: margin-left;
+            transition-duration: 1s;
+            transition-timing-function: linear;
+        }
+        /*Бургер*/
+        .burger {
+            position: fixed;
+            top: 45vh;
+        }
+        /*Анимация ухода навигатора*/
+        .animateback {
+            margin-left: 0;
+            transition-property: margin-left !important;
+            transition-duration: 1s !important;
+            transition-timing-function: linear !important;
+        }
+        /*Лицензия*/
+        .lic {
+            font-family: 'Open Sans';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 128.19%;
+            /* identical to box height, or 18px */
+
+            letter-spacing: 0.09em;
+
+            color: rgba(255, 255, 255, 0.5);
+
+        }
+        /*Лицензия*/
+        .lic {
+            position: fixed;
+            top: 95vh;
+            left: 2vw;
+        }
+        /*Разработано Yaka*/
+        .by {
+            position: fixed;
+            top: 95vh;
+            left: 85vw;
+            font-family: 'Open Sans';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 128.19%;
+            /* identical to box height, or 18px */
+
+            letter-spacing: 0.09em;
+
+            color: rgba(255, 255, 255, 0.5);
+        }
+        /*Линии пагинации*/
+        .line {
+            width: 100px !important;
+            height: 1px !important;
+            border: 2px solid white !important;
+        }
+        /*пагинация слайдов*/
+        .pagination {
+            position: fixed;
+            left: 80vw;
+            top: 40vh;
+            height: 100px;
+            width: 200px;
+        }
+        /*Сектор навигационной панель*/
+        .sec {
+            
         }
     </style>
 </head>
 
 <body>
-    <div class="collapse" id="navbarToggleExternalContent">
-        <div class="bg-dark p-4">
-            <h5 class="text-white h4">Collapsed content</h5>
-            <span class="text-muted">Toggleable via the navbar brand.</span>
+    <!--Пагинация-->
+    <div class="pagination d-block">
+        <a href="#home" class="">
+            <div class="line active"></div>
+        </a>
+        <a href="#ad" class="">
+            <div class="line my-4"></div>
+        </a>
+        <a href="#set" class="">
+            <div class="line"></div>
+        </a>
+    </div>
+    <!--Навигатор-->    
+    <div class="col-3 showed">
+        <!--Секции навигатора-->
+        <div class="row sec">
+
         </div>
     </div>
-
-    <button class="navbar-toggler btn-burger" type="button" data-bs-toggle="collapse"
-        data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <!--Кем сделано-->
+    <div class="row by" style="width: 200px;">
+        <div class="col">
+            <p class="w-100">
+                Разработано в YAKA
+            </p>
+        </div>
+    </div>
+    <!--Когда сделано-->    
+    <div class="row lic">
+        <p class="">
+            <img src="img/c.svg">
+            2022-ACCEL SCIENCE
+        </p>
+    </div>
+    <!--Бургер-->
+    <button class="btn burger" onclick="hshow()">
+        <img src="img/burger.svg" class="burgerimg">
+        <img src="img/krest.svg" style="display: none;" class="krestimg">
     </button>
+    <!--Контент-->
     <div class="container">
-        <div class="row text-center" style="height:100vh;">
+        <!--Слайд 1-->
+        <div class="row text-center panel" style="height:100vh;" data-section-name="home" id="home">
             <div class="col-10 mx-auto" style="height:100vh;">
                 <div class="row" style="height:100vh; color:white;">
                     <div class="col my-auto">
@@ -138,7 +250,7 @@
                                 <p class="slogan">прокачай предпринимательские навыки,
                                     запусти свой стартап,
                                     получи поддержку менторов,
-                                    найтди новых друзей ! </p>
+                                    найти новых друзей ! </p>
                             </div>
                         </div>
                         <div class="row mt-3">
@@ -150,8 +262,47 @@
                 </div>
             </div>
         </div>
-
-        <div class="row" style="height:100vh;">
+        <!--Слайд 2-->
+        <div class="row panel" style="height:100vh;" data-section-name="ad" id="ad">
+            <div class="col-10 mx-auto" style="height:100vh;">
+                <div class="row" style="height:100vh; color:white;">
+                    <div class="col">
+                        <div class="row" style="height: 33vh;">
+                            <div class="col-4 ms-auto">
+                                <div class="row mt-3">
+                                    <h1 class="f-text">$50 млрд</h1>
+                                </div>
+                                <div class="row">
+                                    <p class="s-text">Капитализация компании, построенных нашими менторами </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" style="height: 33vh;">
+                            <div class="col-4">
+                                <div class="row mt-3">
+                                    <h1 class="f-text">2500+</h1>
+                                </div>
+                                <div class="row">
+                                    <p class="s-text">проектов прошли программу</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" style="height: 33vh;">
+                            <div class="col-4 ms-auto">
+                                <div class="row mt-3">
+                                    <h1 class="f-text">$50 млрд</h1>
+                                </div>
+                                <div class="row">
+                                    <p class="s-text">Капитализация компании, построенных нашими менторами </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--Слайд 3-->
+        <div class="row panel" style="height:100vh;" data-section-name="set" id="set">
             <div class="col-10 mx-auto" style="height:100vh;">
                 <div class="row" style="height:100vh; color:white;">
                     <div class="col">
@@ -190,11 +341,41 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+    <script src="script/jquery-2.2.1.min.js"></script>
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
         integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
+    <script src="script/jquery.scrollify.js"></script>
+    <script>
+        let btn = document.querySelector('.burger')
+        let burger = document.querySelector('.burgerimg')
+        let krest = document.querySelector('.krestimg')
+        let showed = document.querySelector('.showed')
+        let bul = true
+
+        function hshow() {
+
+            if (bul == true) {
+                showed.classList.add('animate');
+                showed.classList.remove('animateback');
+                burger.style.display = "none"
+                krest.style.display = "block"
+                bul = false
+            } else {
+                showed.classList.remove('animate');
+                showed.classList.add('animateback');
+                burger.style.display = "block"
+                krest.style.display = "none"
+                bul = true
+            }
+        }
+
+        $.scrollify({
+            section: ".panel",
+            scrollbars: false,
+            scrollSpeed: 100,
+        })
     </script>
 </body>
 
