8c03e8a378557def6a4f1b9e09d7533da1945fce
want74 <want74@rambler.ru>
2022-04-23 06:56:05 +0900

    Убрал лишние файлы

--- del.php
+++
@@ -1,6 +0,0 @@
-<?php 
-//session_start();
-include "connect.php";
-$query1 = mysqli_query($con, "DELETE FROM history WHERE id='{$_POST['id']}'");
-header('location: test.php');
- ?>
--- update.php
+++
@@ -1,6 +0,0 @@
-<?php 
-//session_start();
-include "connect.php";
-$query1 = mysqli_query($con, "UPDATE users SET gradeOfClass = '{$_POST['gradeOfClass']}' WHERE num='{$_POST['num']}'");
-header('location: test.php');
- ?>
